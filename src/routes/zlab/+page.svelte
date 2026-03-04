fb3ddca96142860e732f959685152016070ed9c7
Vojtěch divoký <divokyvojta@gmail.com>
2026-03-04 15:00:15 +0100

    zlab content update #11

--- src/routes/zlab/+page.svelte
+++ src/routes/zlab/+page.svelte
@@ -94,12 +94,8 @@
 			</p>
 		</div>
 		<a
-			href="https://docs.google.com/forms/d/e/1FAIpQLSdRPxeWKcGwU5bPg8fUtF99RaMQb3cc5IaCKbo9XbiZFNrrHA/viewform"
-			target="_blank">{langs[$lang].zlab.soundBtn}</a
-		>
-		<a
-			href="https://docs.google.com/forms/d/e/1FAIpQLSdI8ntWVbH0GcU9wyjrltMIhvd8k2VvZO5toh1_BaL3ger9LA/viewform"
-			target="_blank">{langs[$lang].zlab.lightsBtn}</a
+			href="https://forms.gle/fjL2sabn9Ywt3m3X7"
+			target="_blank">{langs[$lang].zlab.btn}</a
 		>
 	</div>
 </div>
